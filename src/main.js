57f1ba9c71697fcbf353e73951a2f966910c711c
Jack Kartoush <jackkartosh@gmail.com>
2023-06-26 11:15:52 +0200

    addjusting file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
-import NavScroll from 'navscroll';
 
 const Vue = createApp(App);
-// Vue.component('navscroll', NavScroll);
-// Vue.use(NavScroll);
 Vue.mount('#app');
